48fd775ed5283221ef41029f8391d970f3debf41
özge alkan <ozgetanulkualkan@gmail.com>
2023-01-10 15:46:58 +0300

    Add project files.

---
+++ ConsoleApp2/GFG.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+
+        public class GFG
+        {
+
+            // Generics method
+            public void Display<Z>(string msg, Z value)
+            {
+                Console.WriteLine("{0}:{1}", msg, value);
+            }
+        }
+
+        // Driver class
+        public class Example
+        {
+
+            // Main Method
+            public static int Main()
+            {
+
+                // creating object of class GFG
+                GFG p = new GFG();
+
+                // calling Generics method
+                p.Display<int>("Integer", 122);
+                p.Display<char>("Character", 'H');
+                p.Display<double>("Decimal", 255.67);
+                return 0;
+            }
+        }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+
+    }
+}
